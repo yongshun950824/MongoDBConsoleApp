7082edcf3907e873a8c68808a60e3a5b3081e384
yongshun950824 <kokys-wa13@student.tarc.edu.my>
2022-10-28 15:49:42 +0800

    - Add Solution_048

---
+++ MongoDBConsoleApp/Solutions/Solution_048.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDBConsoleApp.Solutions
+{
+    /// <summary>
+    /// <a href="https://stackoverflow.com/questions/74214246/c-sharp-mongodb-get-all-elements-inside-a-bsonarray">
+    /// Question.
+    /// </a>
+    /// </summary>
+    class Solution_048 : ISolution
+    {
+        public void Run(IMongoClient _client)
+        {
+            IMongoDatabase _db = _client.GetDatabase("demo");
+            IMongoCollection<BsonDocument> collection = _db.GetCollection<BsonDocument>("types");
+
+            var filter = Builders<BsonDocument>.Filter
+                .Exists("Types", true);
+            var projection = Builders<BsonDocument>.Projection
+                .Include("Types")
+                .Exclude("_id");
+            var result = collection.Find(filter)
+                .Project(projection)
+                .First();
+
+            List<string> types = ((BsonArray)result["Types"]).Values
+                .Select(x => x.AsString)
+                .ToList();
+
+            Console.WriteLine(String.Join(",", types));
+        }
+
+        public Task RunAsync(IMongoClient _client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
