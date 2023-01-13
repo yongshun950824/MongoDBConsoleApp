628ca7c9d9aa5977a8d04f012bcb26a5b14b2a9a
yongshun950824 <kokys-wa13@student.tarc.edu.my>
2023-01-13 10:48:00 +0800

    - Add Solution_059

---
+++ MongoDBConsoleApp/Solutions/Solution_059.cs
@@ -0,0 +1,141 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static MongoDBConsoleApp.Program;
+
+namespace MongoDBConsoleApp.Solutions
+{
+
+    /// <summary>
+    /// <a href="https://stackoverflow.com/questions/75074400/how-do-i-join-to-a-second-mongo-collection-to-a-child-document-using-linq/75078153#75078153">
+    /// Question.
+    /// </a>
+    /// </summary>
+    internal class Solution_059 : ISolution
+    {
+        public void Run(IMongoClient _client)
+        {
+            Helpers.RegisterCamelCasePack();
+
+            var db = _client.GetDatabase("demo");
+
+            var accountCollection = db.GetCollection<Account>("accounts");
+            var userCollection = db.GetCollection<User>("users");
+
+            var lookupPipeline = new EmptyPipelineDefinition<Account>()
+                .Match(new BsonDocument("$expr",
+                    new BsonDocument("$in", new BsonArray { "$accountId", "$$accountId" })
+                ))
+                .Project<Account, Account, ExtendedUserAccount>(
+                    Builders<Account>.Projection
+                        .Include(x => x.AccountId)
+                        .Include(x => x.AccountCode));
+
+            var extendedUser = userCollection.Aggregate()
+                .Match(u => u.EmailAddress == "foo@bar.com")
+                .Lookup(accountCollection,
+                    new BsonDocument { { "accountId", "$userAccounts.accountId" } },
+                    lookupPipeline,
+                    new ExpressionFieldDefinition<ExtendedUser, IEnumerable<ExtendedUserAccount>>(x => x.UserAccounts)
+                )
+                .FirstOrDefault();
+
+            Console.WriteLine(JsonConvert.SerializeObject(extendedUser, Formatting.Indented));
+        }
+
+        public Task RunAsync(IMongoClient _client)
+        {
+            return Task.Run(() => Run(_client));
+        }
+
+        //public record class User(string UserId, string EmailAddress, IEnumerable<UserAccount> UserAccounts);
+
+        //public record class UserAccount(string AccountId);
+
+        //public record class Account(string AccountId, string AccountCode, IEnumerable<string> UserIds);
+
+        //public record class ExtendedUser(string UserId, string EmailAddress, IEnumerable<ExtendedUserAccount> UserAccounts);
+
+        //public record class ExtendedUserAccount(string AccountId, string AccountCode);
+
+        [BsonNoId]
+        [BsonIgnoreExtraElements]
+        public class User
+        {
+            [BsonConstructor]
+            public User(string UserId, string EmailAddress, IEnumerable<UserAccount> UserAccounts)
+            {
+                this.UserId = UserId;
+                this.EmailAddress = EmailAddress;
+                this.UserAccounts = UserAccounts;
+            }
+
+            public string UserId { get; set; }
+            public string EmailAddress { get; set; }
+            public IEnumerable<UserAccount> UserAccounts { get; set; }
+        }
+
+        public class UserAccount
+        {
+            [BsonConstructor]
+            public UserAccount(string AccountId)
+            {
+                this.AccountId = AccountId;
+            }
+
+            public string AccountId { get; set; }
+        }
+
+        [BsonNoId]
+        [BsonIgnoreExtraElements]
+        public class Account
+        {
+            [BsonConstructor]
+            public Account(string AccountId, string AccountCode, IEnumerable<string> UserIds)
+            {
+                this.AccountId = AccountId;
+                this.AccountCode = AccountCode;
+                this.UserIds = UserIds;
+            }
+
+            public string AccountId { get; set; }
+            public string AccountCode { get; set; }
+            public IEnumerable<string> UserIds { get; set; }
+        }
+
+        [BsonIgnoreExtraElements]
+        public class ExtendedUser
+        {
+            [BsonConstructor]
+            public ExtendedUser(string UserId, string EmailAddress, IEnumerable<ExtendedUserAccount> UserAccounts)
+            {
+                this.UserId = UserId;
+                this.EmailAddress = EmailAddress;
+                this.UserAccounts = UserAccounts;
+            }
+
+            public string UserId { get; set; }
+            public string EmailAddress { get; set; }
+            public IEnumerable<ExtendedUserAccount> UserAccounts { get; set; }
+        }
+
+        [BsonIgnoreExtraElements]
+        public class ExtendedUserAccount
+        {
+            [BsonConstructor]
+            public ExtendedUserAccount(string AccountId, string AccountCode)
+            {
+                this.AccountId = AccountId;
+                this.AccountCode = AccountCode;
+            }
+
+            public string AccountId { get; set; }
+            public string AccountCode { get; set; }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -68,6 +68,7 @@ Written Solutions for StackOverflow questions.
 | [Solution_056][103] | [Need help - Filter list of array based on specific item id using MongoDb C# driver][104] |
 | [Solution_057][105] | [Cannot dynamically create an instance of type 'System.Text.Json.Nodes.JsonObject'. Reason: No parameterless constructor defined.)][106] |
 | [Solution_058][107] | [MongoDB C#/.NET Driver - How to deserialize UUID and ISODate][108] |
+| [Solution_059][109] | [How do I join to a second mongo collection to a child document using Linq][110] |
 
 
 [1]: https://github.com/yongshun950824/MongoDBConsoleApp/blob/master/MongoDBConsoleApp/Solutions/Solution_001.cs
@@ -230,3 +231,6 @@ Written Solutions for StackOverflow questions.
 
 [107]: https://github.com/yongshun950824/MongoDBConsoleApp/blob/master/MongoDBConsoleApp/Solutions/Solution_058.cs
 [108]: https://stackoverflow.com/questions/75067318/mongodb-c-net-driver-how-to-deserialize-uuid-and-isodate/75071541#75071541
+
+[109]: https://github.com/yongshun950824/MongoDBConsoleApp/blob/master/MongoDBConsoleApp/Solutions/Solution_059.cs
+[110]: https://stackoverflow.com/questions/75074400/how-do-i-join-to-a-second-mongo-collection-to-a-child-document-using-linq/75078153#75078153
